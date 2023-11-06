8a8d6858db6f8a5b833f7f98a11ca8a637d3c12b
jiang qiang <jiangtingqiang@gmail.com>
2023-11-06 21:56:53 +0800

    feat: add config reader

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rdpywheel',
-    version='0.1.3',
+    version='0.1.5',
     keywords='wheel',
     description='a library for Python wheel',
     license='MIT License',
