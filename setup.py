837dc6b0d01cee320650f28b9100d6bbebc7c9d9
jiang qiang <jiangtingqiang@gmail.com>
2023-11-06 17:04:48 +0800

    refactor: tweak the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rdpywheel',
-    version='0.0.1',
+    version='0.1.2',
     keywords='wheel',
     description='a library for Python wheel',
     license='MIT License',
