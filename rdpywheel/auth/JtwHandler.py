d21d0a2f3789fa9649cdb89cf5071196162160d7
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-12-01 23:29:54 +0800

    feat: add token expire

--- rdpywheel/auth/JtwHandler.py
+++ rdpywheel/auth/JtwHandler.py
@@ -18,3 +18,14 @@ class JwtHandler:
             return True
         else:
             return False
+
+    @staticmethod
+    def get_toke_expire(access_token):
+        parsed_token = jwt.decode(access_token,
+                                  verify=False,
+                                  algorithms=['HS512'],
+                                  options={
+                                      'verify_signature': False
+                                  })
+        exp = parsed_token["exp"]
+        return exp
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rdpywheel',
-    version='0.1.32',
+    version='0.1.33',
     keywords='wheel',
     description='a library for Python wheel',
     license='MIT License',
